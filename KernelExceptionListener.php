0f0639fb5e8f4122f78d2ed6d17ac18b7cf46043
Kovtunenko Anton <kovtunenko-anton@mail.ru>
2022-01-13 13:26:05 +0300

    Create KernelExceptionListener.php

---
+++ KernelExceptionListener.php
@@ -0,0 +1,32 @@
+namespace Application\BackendBundle\EventListener;
+ 
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+ 
+class KernelExceptionListener
+{
+    private $router;
+    private $redirectRouter = 'application_frontend_default_index';
+ 
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+ 
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+ 
+        if ($exception instanceof NotFoundHttpException) {
+            if ($event->getRequest()->get('_route') == $this->redirectRouter) {
+                return;
+            }
+ 
+            $url = $this->router->generate($this->redirectRouter);
+            $response = new RedirectResponse($url);
+            $event->setResponse($response);
+        }
+    }
+}
